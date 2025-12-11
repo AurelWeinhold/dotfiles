9665732e196588513116cdd04f6cd0687a37618c
Aurel Weinhold <aurel@weinhold.org>
2025-12-11 19:32:51 +0100

    Adjust colors, border, and rounding
    Trying out something new.

--- hypr/.config/hypr/toggle_presentation_mode.sh
+++ hypr/.config/hypr/toggle_presentation_mode.sh
@@ -6,16 +6,14 @@ killall -SIGUSR1 waybar
 # toggle gaps, border, and rounding
 if hyprctl getoption general:gaps_in | grep "0 0 0 0"; then
 	hyprctl --batch "\
-		keyword general:gaps_in 2;\
-		keyword general:gaps_out 4;\
+		keyword general:gaps_in 1;\
+		keyword general:gaps_out 2;\
 		keyword general:border_size 2;\
-		keyword decoration:rounding 16;\
 		"
 else
 	hyprctl --batch "\
 		keyword general:gaps_in 0;\
 		keyword general:gaps_out 0;\
 		keyword general:border_size 0;\
-		keyword decoration:rounding 0;\
 		"
 fi
